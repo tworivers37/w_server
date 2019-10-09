a9afc38bcab45f5034fad1bd248b47c6df812c8a
krquddnr37 <krquddnr37@naver.com>
2019-10-09 23:52:22 +0900

    - 디렉터리 생성 및 기본 파일 생성.

---
+++ Server/Server.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <boost/asio/io_context.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/asio.hpp>
+
+#include "Session.h"
+
+namespace HTTP{
+    class Server{
+
+    };
+};
